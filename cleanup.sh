2df468adf58d82c6b754ef1a9dcb9028c23f1e80
Adarshkumar14 <adarsh@chaosnative.com>
2022-12-01 21:26:43 +0530

    Create cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Caution - This script will delete the namespace litmus "
+kubectl config current-context
+echo "Do you want to cleanup Litmus for this cluster"
+echo  'Press "0" & Enter to continue Or Press "any other key" & Enter to Abort'
+read line
+if [ "$line" -eq "0" ]
+then
+	echo " Cleanup Started" 
+	echo " Deleting ChaosEngines"
+	# kubectl delete chaosengines.litmuschaos.io --all -n hce
+	echo " Deleting ChaosResults"
+	kubectl delete chaosresults.litmuschaos.io --all -n hce
+	echo " Deleting ChaosExperiments"
+	kubectl delete chaosexperiments.litmuschaos.io --all -n hce
+	echo " Deleting HCE Namespace"
+	kubectl delete ns hce
+	echo " Deleting Clusterroles"
+	kubectl get clusterroles | grep -i "argo\|litmus\|subscriber-cluster\|chaos-cluster-role" | awk '{print $1}' | xargs -n 1 kubectl delete clusterroles
+	echo " Deleting Cluster Role Binding"
+	kubectl get clusterrolebinding | grep -i "argo\|litmus\|subscriber-cluster\|chaos-cluster-role-binding" | awk '{print $1}' | xargs -n 1 kubectl delete clusterrolebinding
+	echo " Cleanup Done" 
+else
+	echo " Aborting Cleanup" 
+fi
+exit
